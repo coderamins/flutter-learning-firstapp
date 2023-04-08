02ec2742c6b9c7e8aa97aa0bdb0f90deacc7abb7
RaminS <44978175+coderamins@users.noreply.github.com>
2023-04-08 15:50:13 +0430

    20230408

--- Episode1/lib/main.dart
+++ Episode1/lib/main.dart
@@ -47,7 +47,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromRGBO(255, 255, 243, 243),
+        backgroundColor: const Color.fromRGBO(255, 243, 243, 243),
         appBar: AppBar(
           backgroundColor: Colors.white,
           actions: [
@@ -62,7 +62,7 @@ class Home extends StatelessWidget {
                 child: Align(
                     alignment: Alignment.centerLeft,
                     child: Image.asset("assets/images/menu.png"))),
-            const SizedBox(width: 8),
+            const SizedBox(width: 16),
           ],
         ),
         body: Padding(
@@ -88,31 +88,86 @@ class Home extends StatelessWidget {
                 style: TextStyle(color: Colors.black),
                 textDirection: TextDirection.rtl,
               ),
+              SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 24, 0, 0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 35,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Color.fromARGB(255, 130, 130, 130),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          "نرخ آزاد ارز",
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                        Text(
+                          'قیمت',
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                        Text(
+                          'تغییر',
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
               Container(
-                height: 35,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  color: Color.fromARGB(255, 130, 130, 130),
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      "نرخ آزاد ارز",
-                      style: Theme.of(context).textTheme.headline2,
-                    ),
-                    Text(
-                      'قیمت',
-                      style: Theme.of(context).textTheme.headline2,
-                    ),
-                    Text(
-                      'تغییر',
-                      style: Theme.of(context).textTheme.headline2,
-                    ),
-                  ],
-                ),
+                width: double.infinity,
+                height: 350,
+                child: ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    itemCount: 14,
+                    itemBuilder: (BuildContext context, int position) {
+                      return Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 8, 0, 6),
+                        child: SignalItem(),
+                      );
+                    }),
+                    
               )
             ],
           ),
         ));
+  }
+}
+
+class SignalItem extends StatelessWidget {
+  const SignalItem({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 50,
+      decoration: BoxDecoration(boxShadow: const <BoxShadow>[
+        BoxShadow(blurRadius: 1.0, color: Colors.grey)
+      ], color: Colors.white, borderRadius: BorderRadius.circular(1000)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            "دلار",
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+          Text(
+            "16000",
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+          Text(
+            "+3",
+            style: Theme.of(context).textTheme.bodyText1,
+          )
+        ],
+      ),
+    );
   }
 }
